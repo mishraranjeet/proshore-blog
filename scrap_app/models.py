5bb357a36f7b1c4dbb2b44d2de8bbee79ff74673
ranjeet <ranjeet>
2022-07-23 21:47:19 +0545

    template for blog

--- scrap_app/models.py
+++ scrap_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=500, blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
+    blog_image_url = models.URLField(blank=True, null=True)
+    author_name = models.CharField(max_length=500, null=True, blank=True)
+    author_image_url = models.URLField(blank=True, null=True)
+    designation = models.CharField(max_length=500, null=True, blank=True)
+    reading_time = models.CharField(max_length=200, blank=True, null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id)
+
+    class Meta:
+        db_table = "blog"
+
--- scrap_app/views.py
+++ scrap_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def blog_list(request):
+    query = request.GET.get('q')
+    sort = request.GET.get('sort')
+    print("query:", query, sort)
+    return render(request, 'blog-list.html')
